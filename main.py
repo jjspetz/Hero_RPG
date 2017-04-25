b5f182c7fdfece5e18977cf83c4c711346c67b2d
JJ <jj.spetseris@yahoo.com>
2017-04-25 11:36:57 -0500

    step 7

--- main.py
+++ main.py
@@ -30,58 +30,32 @@ class Zombie(Character):
     def print_status(self):
         print("The {} has {} power and is already dead... but still fighting?".format(self.name, self.power))
 
-# class Hero():
-#     def __init__(self, health, power):
-#         self.health = health
-#         self.power = power
-#     def attack(self,victim):
-#         victim.health -= self.power
-#             print("You do {} damage to the goblin.".format(self.power))
-#             if victim.health <= 0:
-#                 print("The goblin is dead.")
-#     def alive(self):
-#         if self.health > 0:
-#             return True
-#         else:
-#             return False
-#     def print_status(self):
-#         print("You have {} health and {} power.".format(self.health, self.power))
-#
-# class Goblin():
-#     def __init__(self, health, power):
-#         self.health = health
-#         self.power = power
-#     def attack(self, victim):
-#         victim.health -= self.power
-#             print("The goblin does {} damage to you.".format(self.power))
-#             if victim.health <= 0:
-#                 print("You are dead.")
-#     def alive(self):
-#         if self.health > 0:
-#             return True
-#         else:
-#             return False
-#     def print_status(self):
-#         print("The goblin has {} health and {} power.".format(self.health, self.power))
+class Hero(Character):
+    def print_status(self):
+        print("You have {} health and {} power.".format(self.health, self.power))
+
+class Goblin(Character):
+    def print_status(self):
+        print("The goblin has {} health and {} power.".format(self.health, self.power))
 
 def main():
-    hero = Character("JJ", 10, 5)
-    goblin = Character("goblin", 6, 2)
+    hero = Hero("JJ", 10, 5)
+    goblin = Goblin("goblin", 6, 2)
     zombie = Zombie("zombie", 1, 1)
 
-    while zombie.alive() and hero.alive():
+    while goblin.alive() and hero.alive():
         hero.print_status()
-        zombie.print_status()
+        goblin.print_status()
         print()
         print("What do you want to do?")
-        print("1. fight zombie")
+        print("1. fight")
         print("2. do nothing")
         print("3. flee")
         print("> ", end=' ')
         inpt = input()
         if inpt == "1":
             # Hero attacks goblin
-            hero.attack(zombie)
+            hero.attack(goblin)
         elif inpt == "2":
             pass
         elif inpt == "3":
@@ -90,9 +64,9 @@ def main():
         else:
             print("Invalid inpt {}".format(inpt))
 
-        if zombie.alive():
+        if goblin.alive():
             # Goblin attacks hero
-            zombie.attack(hero)
+            goblin.attack(hero)
 
 if __name__ == "__main__":
   main()
